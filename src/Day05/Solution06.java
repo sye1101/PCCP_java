da8104bc2b5030a0ef788347010d11acc6e8807a
sye1101 <shinye1101@naver.com>
2023-08-17 10:50:04 +0900

    230817 1회차 commit

--- src/Day05/Solution06.java
+++ src/Day05/Solution06.java
@@ -3,6 +3,16 @@ import java.util.*;
 class Solution06{
     Stack<Integer> combi = new Stack<>();
     public void DFS(int L, int s, int n, int m){
+        if(L == m) {
+            for(int x:combi) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i = s; i <= n; i++) {
+                combi.push(i);
+                DFS(L+1, i+1, n, m);
+                combi.pop();
+            }
+        }
 
     }
     public void solution(int n, int m){
